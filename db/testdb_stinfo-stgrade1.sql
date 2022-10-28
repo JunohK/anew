b4ad74dc36303418946b90dec35a0d5349261c1d
JunohK <115515214+JunohK@users.noreply.github.com>
2022-10-28 16:16:55 +0900

    d

--- db/testdb_stinfo-stgrade1.sql
+++ db/testdb_stinfo-stgrade1.sql
@@ -1,4 +1,3 @@
-create database testdb;
 show databases;
 use testdb;
 show tables;
@@ -18,12 +17,12 @@ alter table st_grade add constraint pk_stgrade primary key(ST_ID);
 explain st_info;
 explain st_grade;
 
-insert into st_info values (202201, 'LeeGiilDong', 'Game');
-insert into st_info values (202202, 'KimGiilDong', 'Computer');
-insert into st_info values (202203, 'HongGiilDong', 'Game');
+insert into st_info values(202201, 'LeeGilDong', 'Game');
+insert into st_info values(202202, 'KimGilDong', 'Computer');
+insert into st_info values(202203, 'HongGilDong', 'Game');
 select * from st_info;
 
-insert into st_grade values (202201, '80', '70');
-insert into st_grade values (202202, '90', '75');
-insert into st_grade values (202203, '95', '85');
+insert into st_grade values(202201, '80', '70');
+insert into st_grade values(202202, '90', '75');
+insert into st_grade values(202203, '95', '85');
 select * from st_grade;
