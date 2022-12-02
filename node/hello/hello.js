6f2263aa0d62d1c287285884e133aafabd9e0ba1
JunohK <wnsdhaksrua@naver.com>
2022-12-02 01:48:29 +0000

    test

--- node/hello/hello.js
+++ node/hello/hello.js
@@ -5,9 +5,7 @@ app.get('/', function (req, res) {
 	res.send("Hello Node JS~!!")
 })
 
-<<<<<<< HEAD
+
 app.listen(3000,function(){
-	console.log("3000 Port:Server Strarted~!!");
-=======
->>>>>>> e0096d113df2eeb29a9e1019698c735ffb1c7425
+	console.log("3000 Port:Server Strarted~!!")
 })
